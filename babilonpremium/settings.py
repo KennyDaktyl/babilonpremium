a090385db641ea8e0f7ca636b1817abf5465eb62
KennyDaktyl <krakow@miktelgsm.pl>
2019-10-06 20:14:14 +0200

    Settings

--- babilonpremium/settings.py
+++ babilonpremium/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'static/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/media/")
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 LANGUAGE_CODE = 'pl-pl'
 
