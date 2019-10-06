a569a013e5000e8a6b68f680d8b62fcfcfd7a966
KennyDaktyl <krakow@miktelgsm.pl>
2019-10-06 21:39:07 +0200

    Settings

--- babilonpremium/settings.py
+++ babilonpremium/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'babilonpremium.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [os.path.join(SETTINGS_PATH, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
